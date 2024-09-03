a3c791a536844e382c0364dad14119a9749ee77b
YL99 <y.lenge99@gmail.com>
2024-09-03 00:33:40 -0500

    login/registration/appbar/home

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,8 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
+
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config = {
   darkMode: ["class"],
@@ -18,6 +22,9 @@ const config = {
       },
     },
     extend: {
+      boxShadow: {
+        input: `0px 2px 3px -1px rgba(34, 197, 94, 0.1), 0px 1px 0px 0px rgba(34, 197, 94, 0.02), 0px 0px 0px 1px rgba(34, 197, 94, 0.08)`,
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -74,7 +81,18 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), addVariablesForColors],
 } satisfies Config
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
 
 export default config
