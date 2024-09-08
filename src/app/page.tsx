abbe7388e51d926db843a846061fcd1d95c80a37
YL99 <y.lenge99@gmail.com>
2024-09-07 21:52:54 -0500

    lil demo update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -142,7 +142,7 @@ function Login() {
     <button className="p-[3px] relative" onClick={login}>
   <div className="absolute inset-0 bg-gradient-to-r from-cyan-500 to-green-500 rounded-lg" />
   <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
-    Poolin
+    Pool-in
   </div>
 </button>
     
